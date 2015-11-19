0632d64102d1d95af192951b9ce8c9d145f11245
Victor Ramirez <virako.9@gmail.com>
2015-11-19 11:34:11 +0100

    Updated agora-gui-admin with last changes agora-core-view

--- avAdmin/admin-directives/census-field/census-field.js
+++ avAdmin/admin-directives/census-field/census-field.js
@@ -1,5 +1,5 @@
 angular.module('avAdmin')
-  .directive('avCensusField', function() {
+  .directive('avCensusField', function(Authmethod) {
     function link(scope, element, attrs) {
       if (scope.field.type === 'dict') {
         try {
@@ -7,6 +7,23 @@ angular.module('avAdmin')
         } catch(e) {
           scope.dictVal = {};
         }
+      } else if (scope.field.type === 'image') {
+        scope.viewImage = function() {
+          window.mdata = scope.c.metadata;
+          Authmethod.getImage(scope.election.id, scope.c.metadata[scope.field.name])
+            .success(function(data) {
+                $('#imageModalFields').empty();
+                scope.election.census.extra_fields.forEach(function(f) {
+                    if (f.type !== 'image') {
+                        var fi = scope.c.metadata[f.name];
+                        $('#imageModalFields').append(f.name + ': ' + fi + '<br/>');
+                    }
+                });
+                $('#imageModal').find('img').attr('src', data.img);
+                $('#imageModal').modal('toggle');
+            });
+          return false;
+        };
       }
     }
 
