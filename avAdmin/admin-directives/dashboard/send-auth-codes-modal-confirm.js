b173b3f2689ef838e9d7b009610f1d1e67dd707b
Eduardo Robles Elvira <edulix@agoravoting.com>
2016-02-14 18:58:49 +0100

    better management of errors

--- avAdmin/admin-directives/dashboard/send-auth-codes-modal-confirm.js
+++ avAdmin/admin-directives/dashboard/send-auth-codes-modal-confirm.js
@@ -1,6 +1,21 @@
+/**
+ * Confirm message sending modal dialog
+ */
 angular.module('avAdmin')
   .controller('SendAuthCodesModalConfirm',
-    function(ConfigService, $location, $timeout, $scope, $modalInstance, SendMsg, AdminPlugins, election, user_ids, exhtml) {
+    function(
+      ConfigService,
+      $location,
+      $timeout,
+      $scope,
+      $modalInstance,
+      $i18next,
+      SendMsg,
+      AdminPlugins,
+      election,
+      user_ids,
+      exhtml)
+    {
       $scope.election = election;
       $scope.user_ids = user_ids;
       $scope.imsure = false;
@@ -8,23 +23,77 @@ angular.module('avAdmin')
       $scope.steps = SendMsg.steps;
       $scope.loading = false;
 
-      $scope.ok = function () {
+      // contact information
+      $scope.contact = ConfigService.contact;
+
+      /**
+       * @returns the error, if any, when sending auth codes
+       */
+      $scope.error = function()
+      {
+        return SendMsg.scope.error;
+      };
+
+      /**
+       * Translates an error returned by the server. This includes an extension
+       * point so that plugins get a chances of managing custom error codes.
+       */
+      $scope.i18nSendError = function(error)
+      {
+        var fromHook = AdminPlugins.hook(
+          'send-auth-codes-i18n-send-error',
+          {error: error}
+        );
+
+        if (angular.isString(fromHook) && fromHook.length > 0)
+        {
+          return fromHook;
+        }
+
+        var data = {
+          tlf: $scope.contact.tlf,
+          email: $scope.contact.email
+        };
+
+        return $i18next(
+          "avAdmin.dashboard.modals.sendAuthCodes.confirmStep.unknownError",
+          data);
+      };
+
+      /**
+       * Accept and send the messages, then close the dialog if successful
+       */
+      $scope.ok = function ()
+      {
         $scope.loading = true;
         SendMsg.sendAuthCodes()
-          .finally(function() {
+          .finally(function()
+          {
             $modalInstance.close($scope.user_ids);
           });
       };
 
-      $scope.editAuthCodes = function () {
+      /**
+       * Back to edit authentication codes
+       */
+      $scope.editAuthCodes = function ()
+      {
         $modalInstance.close("editAuthCodes");
       };
 
-      $scope.cancel = function () {
+      /**
+       * Close the dialog
+       */
+      $scope.cancel = function ()
+      {
         $modalInstance.dismiss('cancel');
       };
 
-      $scope.exampleMsg = function() {
+      /**
+       * Render the example message with template substitution
+       */
+      $scope.exampleMsg = function()
+      {
         var msg = election.census.config.msg;
         var url = "https://" + $location.host() + "/election/" + election.id + "/public/login";
         msg = msg.replace("__URL__", url);
@@ -32,7 +101,11 @@ angular.module('avAdmin')
         return msg;
       };
 
-      function isMsgComplete() {
+      /**
+       * Check if message contains both __URL__ and __CODE__ as recommended
+       */
+      function isMsgComplete()
+      {
         var re1 = /__URL__/;
         var re2 = /__CODE__/;
         var msg = election.census.config.msg;
@@ -40,13 +113,24 @@ angular.module('avAdmin')
         return (msg.match(re1) && msg.match(re2));
       }
 
-      if (isMsgComplete()) {
+      // set the default value of the flag that specifies that the user is sure
+      // to send an auth message without __URL__ or __CODE__
+      if (isMsgComplete())
+      {
         $scope.imsure = true;
       }
 
-      $scope.$watch(function () {
-        // workarround because the ng-disabled doesn't work for me, I don't
-        // know why
+      // only force user to check the imsure checkbox if the auth message
+      // doesn't contain either __URL__ or __CODE__
+      $scope.showCheckBox = function()
+      {
+        return !isMsgComplete();
+      };
+
+      // workarround because the ng-disabled doesn't work for me, I don't
+      // know why
+      $scope.$watch(function ()
+      {
         if (!$scope.loading && (isMsgComplete() || $("#imsure:checked").length)) {
             $("#sendbutton").removeAttr("disabled");
         } else {
@@ -54,15 +138,13 @@ angular.module('avAdmin')
         }
       });
 
-      $scope.showCheckBox = function() {
-        return !isMsgComplete();
-      };
-
+      // use a click to an element with a specific class to close the dialog
       $timeout(function() {
-        $(".av-plugin-modal-close").click(function() {
-            var data = $(this).data("response");
-            $modalInstance.close(data);
-            return false;
+        $(".av-plugin-modal-close").click(function()
+        {
+          var data = $(this).data("response");
+          $modalInstance.close(data);
+          return false;
         });
       }, 0);
 
