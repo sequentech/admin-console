3cd8fa5e491fdda252e515982b4eab1b85cc2bf6
Findeton <felrobelv@gmail.com>
2016-07-14 14:42:09 +0200

    add ng-if with showSuccessAction

--- avAdmin/admin-directives/elcensus/elcensus.js
+++ avAdmin/admin-directives/elcensus/elcensus.js
@@ -50,6 +50,7 @@ angular.module('avAdmin')
       scope.filterOptions = {};
       scope.resizeSensor = null;
       scope.helpurl = ConfigService.helpUrl;
+      $scope.showSuccessAction = ConfigService.showSuccessAction;
 
       function newElection() {
         return !$stateParams.id;
