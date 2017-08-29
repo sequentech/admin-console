63a60fab762e0d37792283c48d7aec67a68afda6
Findeton <felix@nvotes.com>
2017-08-29 10:19:11 +0200

    fix signup link

--- avAdmin/admin-head-directive/admin-head-directive.js
+++ avAdmin/admin-head-directive/admin-head-directive.js
@@ -27,6 +27,7 @@ angular.module('avAdmin')
         scope.technology = ConfigService.technology;
         scope.nologin = ('nologin' in attrs) || scope.admin;
         scope.helpurl = ConfigService.helpUrl;
+        scope.signupLink = ConfigService.signupLink;
 
         scope.loginrequired = ('loginrequired' in attrs);
         if (scope.loginrequired && !scope.admin) {
