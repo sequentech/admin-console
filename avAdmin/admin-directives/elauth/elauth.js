c790e23128fdcbdd49a120af08e388fb587b9a9d
Eduardo Robles Elvira <edulix@nvotes.com>
2021-05-12 07:43:08 +0200

    adding smart link support in i18n and improving formatting

--- avAdmin/admin-directives/elauth/elauth.js
+++ avAdmin/admin-directives/elauth/elauth.js
@@ -15,77 +15,113 @@
  * along with agora-gui-admin.  If not, see <http://www.gnu.org/licenses/>.
 **/
 
-angular.module('avAdmin')
-  .directive('avAdminElauth', ['ConfigService', 'ElectionsApi', 'NextButtonService', 'Plugins', function(ConfigService, ElectionsApi, NextButtonService, Plugins) {
-    // we use it as something similar to a controller here
-    function link(scope, element, attrs) {
-        scope.election = ElectionsApi.currentElection;
-        scope.auth = ConfigService.auth_methods;
-        scope.electionEditable = function() {
-          return !scope.election.id || scope.election.status === "registered";
-        };
-        scope.goNext = NextButtonService.goNext;
-        scope.$watch('election.census.auth_method', function (newVal, oldVal) {
-          // check if any voter in the census has the authType field defined
-          function censusFilledAuthType(authType) {
-            var ret = false;
-            if (_.isArray(scope.election.census.voters) &&
-                0 < scope.election.census.voters.length) {
-              var index = 0;
-              for (; index < scope.election.census.voters.length; index++) {
-                var censusEl = scope.election.census.voters[index];
-                if (_.isObject(censusEl.metadata) && 
-                    _.isString(censusEl.metadata[authType]) &&
-                    censusEl.metadata[authType].length > 0) {
-                  ret = true;
-                  break;
+angular
+  .module('avAdmin')
+  .directive(
+    'avAdminElauth', 
+    [
+      'ConfigService',
+      'ElectionsApi', 
+      'NextButtonService', 
+      'Plugins', 
+      function(ConfigService, ElectionsApi, NextButtonService, Plugins)
+      {
+        // we use it as something similar to a controller here
+        function link(scope, element, attrs) 
+        {
+          scope.election = ElectionsApi.currentElection;
+          scope.auth = ConfigService.auth_methods;
+          scope.electionEditable = function() {
+            return !scope.election.id || scope.election.status === "registered";
+          };
+          scope.goNext = NextButtonService.goNext;
+          scope.$watch(
+            'election.census.auth_method', 
+            function (newVal, oldVal) 
+            {
+              // check if any voter in the census has the authType field defined
+              function censusFilledAuthType(authType) 
+              {
+                var ret = false;
+                if (
+                  _.isArray(scope.election.census.voters) &&
+                  0 < scope.election.census.voters.length
+                ) {
+                  var index = 0;
+                  for (; index < scope.election.census.voters.length; index++) 
+                  {
+                    var censusEl = scope.election.census.voters[index];
+                    if (
+                      _.isObject(censusEl.metadata) && 
+                      _.isString(censusEl.metadata[authType]) &&
+                      censusEl.metadata[authType].length > 0
+                    ) {
+                      ret = true;
+                      break;
+                    }
+                  }
+                }
+                return ret;
+              }
+
+              function removeRequiredExtra(name) 
+              {
+                if(!censusFilledAuthType(name)) 
+                {
+                  // remove the name extra field
+                  scope.election.census.extra_fields = _
+                    .filter(
+                      scope.election.census.extra_fields,
+                      function (ef) { return name !== ef.name; }
+                    );
+                  // no census data is lost if we remove the name field from 
+                  // voters, so we do that
+                  _.each(
+                    scope.election.census.voters, 
+                    function (voter) 
+                    {
+                      if (_.isObject(voter.metadata))
+                      {
+                        delete voter.metadata[name];
+                      }
+                    }
+                  );
+                } else {
+                  _.each(
+                    scope.election.census.extra_fields, 
+                    function (ef) { ef.must = false; }
+                  );
                 }
               }
-            }
-            return ret;
-          }
 
-          function removeRequiredExtra(name) {
-            if(!censusFilledAuthType(name)) {
-              // remove the name extra field
-              scope.election.census.extra_fields = 
-                _.filter(
-                  scope.election.census.extra_fields,
-                  function (ef) {
-                    return name !== ef.name;
-                  });
-              // no census data is lost if we remove the name field from 
-              // voters, so we do that
-              _.each(
-                scope.election.census.voters, 
-                function (voter) {
-                  if (_.isObject(voter.metadata)) {
-                    delete voter.metadata[name];
+              scope.$evalAsync(
+                function () 
+                {
+                  if (newVal !== oldVal) 
+                  {
+                    if ('email' !== newVal) 
+                    {
+                      removeRequiredExtra("email");
+                    } 
+                    if ('sms' !== newVal) 
+                    {
+                      removeRequiredExtra("tlf");
+                    }
                   }
-                });
-            } else {
-              _.each(scope.election.census.extra_fields, function (ef) { ef.must = false; });
-            }
-          }
+                }
+              );
+            }, 
+            true
+          );
+        }
 
-          scope.$evalAsync(function () {
-            if (newVal !== oldVal) {
-              if ('email' !== newVal) {
-                removeRequiredExtra("email");
-              } 
-              if ('sms' !== newVal) {
-                removeRequiredExtra("tlf");
-              }
-            }
-          });
-        }, true);
-    }
-
-    return {
-      restrict: 'AE',
-      scope: {
-      },
-      link: link,
-      templateUrl: 'avAdmin/admin-directives/elauth/elauth.html'
-    };
-  }]);
+        return {
+          restrict: 'AE',
+          scope: {
+          },
+          link: link,
+          templateUrl: 'avAdmin/admin-directives/elauth/elauth.html'
+        };
+      }
+    ]
+  );
