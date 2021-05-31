b3a0d54e6787c34817e2cdc1c5b1d30b28a7a1f5
Eduardo Robles Elvira <edulix@nvotes.com>
2021-05-31 18:19:51 +0200

    showing write-ins as such

--- avAdmin/admin-directives/dashboard/dashboard.js
+++ avAdmin/admin-directives/dashboard/dashboard.js
@@ -35,6 +35,19 @@ angular.module('avAdmin')
     {
       scope.reloadTimeout = null;
 
+      scope.isWriteInResult = function(answer)
+      {
+        const u = _.find(
+          answer.urls,
+          function(urlObject)
+          {
+            return urlObject.title === 'isWriteInResult' && urlObject.url === 'true';
+          }
+        );
+
+        return !!u;
+      }
+
       function updateDoingTallyFlag(el) 
       {
 
