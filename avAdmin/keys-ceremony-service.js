9331c0e5b15907ea26b8a46e6328d2b95d64a94c
Félix Robles <felix@sequentech.io>
2022-11-18 18:53:45 -0500

    Add keys ceremonies (#314) (#321)
    * adding ceremony modals wip
    
    * add controllers to index.html
    
    * wip
    
    * add keys ceremony service
    
    * wip
    
    * wip
    
    * lint
    
    * fixes
    
    * fixes
    
    * wip
    
    * wip
    
    * wip
    
    * css fixes
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * no help
    
    * wip
    
    * fix
    
    * wip
    
    * wip
    
    * check share wip
    
    * maybe fix promises
    
    * wip
    
    * wip
    
    * remove timeout
    
    * wip
    
    * wip
    
    * wip
    
    * fixes
    
    * base64 codec service
    
    * fix check share
    
    * delete share wip
    
    * fixes
    
    * wip
    
    * fix
    
    * fixes
    
    * fixes
    
    * fix
    
    * secure share
    
    * wip
    
    * wip back button
    
    * fix
    
    * fixes
    
    * wip
    
    * css fixes
    
    * fixes
    
    * fixes
    
    * wip
    
    * opening ceremony
    
    * wip
    
    * login trustee
    
    * restore keys modal
    
    * reset errors
    
    * fixes
    
    * wip
    
    * wip
    
    * fix
    
    * wip
    
    * fixes
    
    * css fixes
    
    * wip
    
    * wip
    
    * wip
    
    * finish modal
    
    * fix
    
    * fix
    
    * wip
    
    * wip

---
+++ avAdmin/keys-ceremony-service.js
@@ -0,0 +1,437 @@
+/**
+ * This file is part of admin-console.
+ * Copyright (C) 2022  Sequent Tech Inc <legal@sequentech.io>
+
+ * admin-console is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License.
+
+ * admin-console  is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+
+ * You should have received a copy of the GNU Affero General Public License
+ * along with admin-console.  If not, see <http://www.gnu.org/licenses/>.
+**/
+
+/**
+ * Service to manage the keys ceremony modal steps.
+ */
+angular
+.module('avAdmin')
+.factory('KeysCeremony', function($q, $modal)
+{
+  var service = {
+    // keys-distribution or opening
+    ceremony: null,
+    // reference to the election in which authentication messages are going
+    // to be sent
+    election: null,
+
+    trusteesLogin: {
+      /*
+       * "trustee_id": {
+       *   "username": "username",
+       *   "password": "password"
+       * }
+       */
+    },
+    trusteesPrivateKeyShareFile: {
+      /*
+       * "trustee_id": File,
+       */
+    },
+
+    /** step methods:
+     *   - key-distribution
+     *   - login
+     *   - download-share
+     *   - secure-share
+     *   - check-share
+     *   - delete-share
+     *   - restore-share
+     */ 
+
+    /**
+     * steps format:
+     * steps = [
+     *  {
+     *    stepMethod: 'key-distribution',
+     *    trusteeId: null,
+     *  }
+     * ]
+     */
+
+    steps: [],
+  };
+
+  /**
+   * Sets the election related to this service
+   */
+  service.setElection = function(el)
+  {
+    service.election = el;
+  };
+
+  function launchKeyDistributionInitialModal(numSteps) {
+    return $modal
+    .open({
+      templateUrl: "avAdmin/admin-directives/dashboard/key-distribution-ceremony-modal.html",
+      controller: "KeyDistributionCeremonyModal",
+      size: 'lg',
+      resolve: {
+        dialogName: function () { return 'launchKeyDistributionCeremony'; },
+        data: function() 
+        {
+          return {
+            election: service.election,
+            numSteps: numSteps,
+          };
+        }
+      }
+    }).result;
+  }
+
+  function launchOpeningInitialgModal(numSteps) {
+    return $modal
+    .open({
+      templateUrl: "avAdmin/admin-directives/dashboard/opening-ceremony-modal.html",
+      controller: "OpeningCeremonyModal",
+      size: 'lg',
+      resolve: {
+        dialogName: function () { return 'launchOpeningCeremony'; },
+        data: function() 
+        {
+          return {
+            election: service.election,
+            numSteps: numSteps,
+          };
+        }
+      }
+    }).result;
+  }
+
+  function launchTrusteeLoginModal(trusteeId, numSteps, currentStep) {
+    return $modal
+    .open({
+      templateUrl: "avAdmin/admin-directives/dashboard/login-trustee-ceremony-modal.html",
+      controller: "LoginTrusteeCeremonyModal",
+      size: 'lg',
+      resolve: {
+        dialogName: function () { return 'loginTrusteeCeremony'; },
+        data: function() 
+        {
+          return {
+            election: service.election,
+            trusteeId: trusteeId,
+            numSteps: numSteps,
+            currentStep: currentStep
+          };
+        }
+      }
+    }).result;
+  }
+
+  function launchDownloadShareModal(trusteeId, numSteps, currentStep) {
+    return $modal
+    .open({
+      templateUrl: "avAdmin/admin-directives/dashboard/download-share-ceremony-modal.html",
+      controller: "DownloadShareCeremonyModal",
+      size: 'lg',
+      resolve: {
+        dialogName: function () { return 'downloadShareCeremony'; },
+        data: function() 
+        {
+          return {
+            election: service.election,
+            trusteeId: trusteeId,
+            username: service.trusteesLogin[trusteeId].username,
+            password: service.trusteesLogin[trusteeId].password,
+            numSteps: numSteps,
+            currentStep: currentStep
+          };
+        }
+      }
+    }).result;
+  }
+
+  function launchSecureShareModal(trusteeId, numSteps, currentStep) {
+    return $modal
+    .open({
+      templateUrl: "avAdmin/admin-directives/dashboard/secure-share-ceremony-modal.html",
+      controller: "SecureShareCeremonyModal",
+      size: 'lg',
+      resolve: {
+        dialogName: function () { return 'secureShareCeremony'; },
+        data: function() 
+        {
+          return {
+            election: service.election,
+            trusteeId: trusteeId,
+            numSteps: numSteps,
+            currentStep: currentStep
+          };
+        }
+      }
+    }).result;
+  }
+
+  function launchCheckShareModal(trusteeId, numSteps, currentStep, ceremony) {
+    return $modal
+    .open({
+      templateUrl: "avAdmin/admin-directives/dashboard/check-share-ceremony-modal.html",
+      controller: "CheckShareCeremonyModal",
+      size: 'lg',
+      resolve: {
+        dialogName: function () { return 'checkShareCeremony'; },
+        data: function() 
+        {
+          return {
+            election: service.election,
+            trusteeId: trusteeId,
+            username: service.trusteesLogin[trusteeId].username,
+            password: service.trusteesLogin[trusteeId].password,
+            numSteps: numSteps,
+            ceremony: ceremony,
+            currentStep: currentStep
+          };
+        }
+      }
+    }).result;
+  }
+
+  function launchDeleteShareModal(trusteeId, numSteps, currentStep) {
+    return $modal
+    .open({
+      templateUrl: "avAdmin/admin-directives/dashboard/delete-share-ceremony-modal.html",
+      controller: "DeleteShareCeremonyModal",
+      size: 'lg',
+      resolve: {
+        dialogName: function () { return 'deleteShareCeremony'; },
+        data: function() 
+        {
+          return {
+            election: service.election,
+            trusteeId: trusteeId,
+            username: service.trusteesLogin[trusteeId].username,
+            password: service.trusteesLogin[trusteeId].password,
+            numSteps: numSteps,
+            currentStep: currentStep,
+            privateKeyShareFile: service.trusteesPrivateKeyShareFile[trusteeId]
+          };
+        }
+      }
+    }).result;
+  }
+
+  function launchRestoreShareModal(trusteeId, numSteps, currentStep) {
+    return $modal
+    .open({
+      templateUrl: "avAdmin/admin-directives/dashboard/restore-share-ceremony-modal.html",
+      controller: "RestoreShareCeremonyModal",
+      size: 'lg',
+      resolve: {
+        dialogName: function () { return 'restoreShareCeremony'; },
+        data: function() 
+        {
+          return {
+            election: service.election,
+            trusteeId: trusteeId,
+            username: service.trusteesLogin[trusteeId].username,
+            password: service.trusteesLogin[trusteeId].password,
+            numSteps: numSteps,
+            currentStep: currentStep,
+            privateKeyShareFile: service.trusteesPrivateKeyShareFile[trusteeId]
+          };
+        }
+      }
+    }).result;
+  }
+
+  function launchFinishCeremonyModal(numSteps, ceremony) {
+    return $modal
+    .open({
+      templateUrl: "avAdmin/admin-directives/dashboard/finish-ceremony-modal.html",
+      controller: "FinishCeremonyModal",
+      size: 'lg',
+      resolve: {
+        dialogName: function () { return 'finishCeremony'; },
+        data: function() 
+        {
+          return {
+            election: service.election,
+            numSteps: numSteps,
+            ceremony: ceremony,
+          };
+        }
+      }
+    }).result;
+  }
+
+  function PromiseResolve(value) {
+    var deferred = $q.defer();
+    var promise = deferred.promise;
+    deferred.resolve(value);
+
+    return promise;
+  }
+
+  function launchSteps(index) {
+    var step = service.steps[index];
+    var numSteps = service.steps.length;
+    
+    if (!step) {
+      return PromiseResolve();
+    }
+
+    switch (step.stepMethod) {
+      case 'key-distribution':
+        return launchKeyDistributionInitialModal(numSteps)
+          .then(function(_result) { return launchSteps(index + 1); });
+      case 'opening':
+        return launchOpeningInitialgModal(numSteps)
+          .then(function(_result) { return launchSteps(index + 1); });
+      case 'login':
+        return launchTrusteeLoginModal(step.trusteeId, numSteps, index + 1)
+          .then(function (res) {
+            service.trusteesLogin[step.trusteeId] = res;
+            return launchSteps(index + 1);
+          });
+      case 'download-share':
+        return launchDownloadShareModal(step.trusteeId, numSteps, index + 1)
+          .then(function (res) {
+            return launchSteps(index + 1);
+          });
+      case 'secure-share':
+        return launchSecureShareModal(step.trusteeId, numSteps, index + 1)
+          .then(function (res) {
+            return launchSteps('back' === res? index - 1: index + 1);
+          });
+      case 'check-share':
+        return launchCheckShareModal(step.trusteeId, numSteps, index + 1, service.ceremony)
+          .then(function (res) {
+            if ('back' === res && 'keys-distribution' === service.ceremony) {
+              return launchSteps(index - 2);
+            } else {
+              service.trusteesPrivateKeyShareFile[step.trusteeId] = res;
+              return launchSteps(index + 1);
+            }
+          });
+      case 'delete-share':
+        return launchDeleteShareModal(step.trusteeId, numSteps, index + 1)
+          .then(function (res) {
+            return launchSteps(index + 1);
+          });
+      case 'restore-share':
+        return launchRestoreShareModal(step.trusteeId, numSteps, index + 1)
+          .then(function (res) {
+            return launchSteps(index + 1);
+          });
+      case 'finish-ceremony':
+        return launchFinishCeremonyModal(numSteps, service.ceremony)
+          .then(function (res) {
+            return launchSteps(index + 1);
+          });
+      default:
+        return PromiseResolve();
+    }
+  }
+
+  service.launchKeyDistributionCeremony = function (election) {
+    service.setElection(election);
+    service.ceremony = 'keys-distribution';
+
+    service.steps = [
+      {
+        stepMethod: 'key-distribution',
+        trusteeId: null,
+      }
+    ];
+
+    var authorities = election.auths.filter(function (trustee) {
+      return undefined === election.trusteeKeysState.find(function (el){ return el.id === trustee && el.state === "deleted"; });
+    });
+
+    var authSteps = authorities.map(function (trusteeId) {
+      return [
+        {
+          stepMethod: 'login',
+          trusteeId: trusteeId,
+        },
+        {
+          stepMethod: 'download-share',
+          trusteeId: trusteeId,
+        },
+        {
+          stepMethod: 'secure-share',
+          trusteeId: trusteeId,
+        },
+        {
+          stepMethod: 'check-share',
+          trusteeId: trusteeId,
+        },
+        {
+          stepMethod: 'delete-share',
+          trusteeId: trusteeId,
+        },
+      ];
+    }).flat();
+
+    service.steps = service.steps.concat(authSteps);
+
+    service.steps.push(
+      {
+        stepMethod: 'finish-ceremony',
+      }
+    );
+
+    return launchSteps(0);
+  };
+
+
+  service.launchOpeningCeremony = function (election) {
+    service.setElection(election);
+    service.ceremony = 'opening';
+
+    service.steps = [
+      {
+        stepMethod: 'opening',
+        trusteeId: null,
+      }
+    ];
+
+    var authorities = election.auths.filter(function (trustee) {
+      return undefined !== election.trusteeKeysState.find(function (el){ return el.id === trustee && el.state === "deleted"; });
+    });
+
+    var authSteps = authorities.map(function (trusteeId) {
+      return [
+        {
+          stepMethod: 'login',
+          trusteeId: trusteeId,
+        },
+        {
+          stepMethod: 'check-share',
+          trusteeId: trusteeId,
+        },
+        {
+          stepMethod: 'restore-share',
+          trusteeId: trusteeId,
+        },
+      ];
+    }).flat();
+
+    service.steps = service.steps.concat(authSteps);
+
+    service.steps.push(
+      {
+        stepMethod: 'finish-ceremony',
+      }
+    );
+
+    return launchSteps(0);
+  };
+
+  return service;
+});
