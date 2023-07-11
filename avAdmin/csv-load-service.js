e2680fd656dde45f8eab35c9106e5d799557327a
Findeton <felix@sequentech.io>
2023-07-11 14:19:55 -0500

    wip

--- avAdmin/csv-load-service.js
+++ avAdmin/csv-load-service.js
@@ -132,12 +132,7 @@ angular.module('avAdmin')
         try {
           // this hook can avoid the addCensus call
           if (Plugins.hook('add-to-census-pre', census)) {
-            Authmethod.launchPingDaemon(id)
-              .then(
-                function onSuccess(response) {
-                  return Authmethod.addCensus(id, census, opt);
-                }
-              )
+            Authmethod.addCensus(id, census, opt)
               .then(
                 function onSuccess(response) {
                   Plugins.hook('add-to-census-success', {data: census, response: response.data});
