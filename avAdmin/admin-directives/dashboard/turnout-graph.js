1a453813274de2cfc9943a6fdca652046364e55f
Findeton <felix@sequentech.io>
2024-04-04 06:59:16 -0500

    wip

--- avAdmin/admin-directives/dashboard/turnout-graph.js
+++ avAdmin/admin-directives/dashboard/turnout-graph.js
@@ -344,7 +344,8 @@ angular.module('avAdmin')
         scope.$watch('id', updateTurnoutData);
         scope.$watch('selectedSeries', refreshGraph, true);
         scope.$watch('timeBasis.value', calculateValues, true);
-        scope.$watch('selectedDates', updateDates, true);
+        scope.$watch('selectedDates.minDate', updateDates, true);
+        scope.$watch('selectedDates.maxDate', updateDates, true);
       }
 
       return {
